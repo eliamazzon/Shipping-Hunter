7934d794db53f3fdfac43b8d15a348f2e6b57a95
Elia Mazzon <ailenozzam@gmail.com>
2020-09-24 18:12:40 +0200

    ebay function isolated

--- main.py
+++ main.py
@@ -7,9 +7,47 @@ import grequests
 from fast_soup import FastSoup
 from multiprocessing import Process, Queue
 import multiprocessing
+    
+def ebay(kws):    
+    def main(kws,ipg,pgn):
+        #start_time1 = time.time()
+        page = requests.get2str(f"https://www.ebay.com/sch/i.html?_nkw={kws}&_ipg={ipg}&_pgn={pgn}")
+    
+        soup = FastSoup(page) 
+        a = soup.find_all('a', class_='s-item__link')
+        links = [l.get("href") for l in a] 
+        print(len(links))
+        
+        reqs = (grequests.get(link) for link in links)
+        pages = grequests.map(reqs)
 
-def range_adapter(rango1): #let's find a better way'
+        n = 0
+        itms = {}
+        for p in pages:
 
+            item_l = links[n].split("/")
+            item = item_l[item_l.index("itm")+1]
+                
+            soup = FastSoup(p.text) 
+            span = soup.find('span', class_='vi-acc-del-range')
+            try:
+                b = span.find("b")  
+                delivery = (b.get_text()).split(" ")
+                rango1 = [delivery[1],delivery[2],delivery[5],delivery[6]]
+                rng = range_adapter(rango1)
+                rango1[0] = rng[0]
+                rango1[2] = rng[1]
+                itms[item] = rango1 #item_name & shipping_date 
+                #added to the dictionary
+            except Exception as e: 
+                print(e)
+                pass
+            n += 1 #check needed: does n+1 skip when excpetion pass?
+            
+        Q.put(itms)
+        #print("--- %s seconds ---" % (time.time() - start_time1))
+    
+    def range_adapter(rango1): #let's find a better way'
         calendar = {
             'Jan.' : 1,
             'Feb.' : 2,
@@ -28,66 +66,27 @@ def range_adapter(rango1): #let's find a better way'
         r1_2 = calendar[rango1[2]]
         
         return r1_0, r1_2
-
-def ebay(kws,ipg,pgn):
-    #start_time1 = time.time()
-    page = requests.get2str(f"https://www.ebay.com/sch/i.html?_nkw={kws}&_ipg={ipg}&_pgn={pgn}")
-   
-    soup = FastSoup(page) 
-    a = soup.find_all('a', class_='s-item__link')
-    links = [l.get("href") for l in a] 
-    print(len(links))
     
-    reqs = (grequests.get(link) for link in links)
-    pages = grequests.map(reqs)
+    Q = Queue()
+    processes = []
+    for pgn in range(10):
+        p = multiprocessing.Process(target = main,args = (kws, 25, pgn+1))
+        p.start()
+        processes.append(p)
 
-    n = 0
-    itms = {}
-    for p in pages:
-
-        item_l = links[n].split("/")
-        item = item_l[item_l.index("itm")+1]
-            
-        soup = FastSoup(p.text) 
-        span = soup.find('span', class_='vi-acc-del-range')
-        try:
-            b = span.find("b")  
-            delivery = (b.get_text()).split(" ")
-            rango1 = [delivery[1],delivery[2],delivery[5],delivery[6]]
-            rng = range_adapter(rango1)
-            rango1[0] = rng[0]
-            rango1[2] = rng[1]
-            itms[item] = rango1 #item_name & shipping_date 
-            #added to the dictionary
-        except Exception as e: 
-            print(e)
-            pass
-        n += 1 #check needed: does n+1 skip when excpetion pass?
+    items = {}
+    for process in processes:
+        process.join()
+        items.update(Q.get())
         
-    Q.put(itms)
-    #print("--- %s seconds ---" % (time.time() - start_time1))
-    
-Q = Queue()
+    sort_items = sorted(items.items(), key=lambda x: x[1])
+    #for i in sort_items:
+        #print(i[0],f"\n{i[1]}", "\n")
+        
+    return items
 
-kws = "iphone"#input("type keywords here: ")
+
 start_time = time.time()
-
-processes = []
-for pgn in range(10):
-    p = multiprocessing.Process(target = ebay,args = (kws, 25, pgn+1))
-    p.start()
-    processes.append(p)
-
-items = {}
-for process in processes:
-    process.join()
-    items.update(Q.get())
-    
-sort_items = sorted(items.items(), key=lambda x: x[1])
-#for i in sort_items:
-    #print(i[0],f"\n{i[1]}", "\n")
-    
-print(len(items))
+print(len(ebay("iphone")))
 print("--- %s seconds ---" % (time.time() - start_time))
-
 
